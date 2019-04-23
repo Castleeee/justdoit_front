a79378bd544a20af3caa10de39feb1178c2187c6
“owl <576359186@qq.com>
2019-04-23 15:00:11 +0800

    test data -1

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Login from './views/Login.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/Login',
+      name: 'login',
+      component: Login
     },
     {
       path: '/about',
---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div style="height: 960px">
+    <!--<img src="../assets/ocean-turtle.jpg" style='height: 100%;width:100%;'>-->
+    <Card style="margin: 5% auto;height: 60%;width:40%;">
+      <p slot="title"
+         v-if="singup" >
+        注册
+      </p>
+      <p slot="title"
+         v-else>
+        登录
+      </p>
+      <div>
+        <Button v-show="singup">登录</Button>
+        <Button @click="switchsing">注册</Button>
+
+      </div>
+
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'login',
+  data () {
+    return {
+      singup: true
+    }
+  },
+  methods: {
+    switchsing () {
+      this.singup = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+#loginbackground{
+  margin-bottom: 0px;
+  height: 100%;
+  width: 100%;
+  background: url("../assets/ocean-turtle.jpg");
+}
+</style>
