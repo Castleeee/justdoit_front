95d58fba1b4d5271fae670684e90c6ca2d3f6aaf
“owl <576359186@qq.com>
2019-04-26 11:25:40 +0800

    shou yekuaiwanle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,16 @@
   <div class="home1">
     <div class="home2">
 
-    <Row class="r" style="padding-top: 3.5%">
-      <Col span="12"><img class="logo" src="../assets/paper-plane.svg" alt=" ：）" /></Col>
-
-      <Col span="2"><U  style="color:#2d8cf0" class="hrefTexe">Home</U></Col>
-      <Col span="2"><U  style="color:#2d8cf0" class="hrefTexe">打卡签到</U></Col>
-      <Col span="2"><U  class="hrefTexe">今日单词</U></Col>
-      <Col span="2"><U  class="hrefTexe">信息推送</U></Col>
+    <Row type="flex" align="middle" style="height: 18%" >
+      <Col span="1">&nbsp;</Col>
+      <Col span="1"><img class="logo" src="../assets/paper-plane.svg" alt=" ：）" /></Col>
+      <Col span="1">&nbsp;</Col>
+      <Col span="4"><B  style="color:#2b85e4;font-size: 39px" class="hrefTexe">Just Do It</B></Col>
+      <Col span="5">&nbsp;</Col>
+      <Col span="2"><B  style="color:#2b85e4" class="hrefTexe">Home</B></Col>
+      <Col span="2"><B   class="hrefTexe">打卡签到</B></Col>
+      <Col span="2"><B  class="hrefTexe">今日单词</B></Col>
+      <Col span="2"><B  class="hrefTexe">信息推送</B></Col>
 
       <Col span="3">
         <Button  class="button" type="primary" shape="circle"  @click="gologin">
@@ -17,22 +20,23 @@
       <Col span="1"></Col>
     </Row>
     <br/>
-    <Row class="r">
-      <Col span="8">&nbsp;</Col>
+    <Row type="flex" align="bottom" style="height: 40%">
+      <Col span="1">&nbsp;</Col>
+      <Col span="8">aaaaaaaaaa<br/>aaaaaaaaa<br/>aaaaaaaaaaaaaaaaaaaaa</Col>
       <Col span="8">&nbsp;</Col>
       <Col span="8">&nbsp;</Col>
     </Row>
     <br/>
-    <Row class="r">
-      <Col span="2" >&nbsp;</Col>
-      <Col span="2" ><Button icon='md-contact' class="buttoncenter" type="primary" shape="circle">登录</Button></Col>
-      <Col span="2" ><Button icon='ios-hammer' class="buttoncenter" shape="circle">作者的blog</Button></Col>
-      <Col span="2" ><Button icon='md-bookmarks' class="buttoncenter" shape="circle">使用说明</Button></Col>
-      <Col span="2" ><Button icon='logo-github' class="buttoncenter" shape="circle">github</Button></Col>
-      <Col span="14"></Col>
+    <Row style="height: 20%">
+      <Col span="1" >&nbsp;</Col>
+      <Col span="2" ><Button icon='md-contact' class="buttoncenter" type="primary" shape="circle" to="/login">登录</Button></Col>
+      <Col span="2" ><Button icon='ios-hammer' class="buttoncenter" shape="circle" to="http:///www.ooowl.fun">作者的blog</Button></Col>
+      <Col span="2" ><Button icon='md-bookmarks' class="buttoncenter" shape="circle" to="http:///www.ooowl.fun:82">使用说明</Button></Col>
+      <Col span="2" ><Button icon='logo-github' class="buttoncenter" shape="circle" to="https:///github.com/Castleeee/justdoit_front">github</Button></Col>
+      <Col span="15"></Col>
 
     </Row>
-      <Row class="r">
+      <Row >
         <Col span="6">&nbsp;</Col>
         <Col span="6">&nbsp;</Col>
         <Col span="6">&nbsp;</Col>
@@ -75,10 +79,10 @@ export default {
     background-size: 22%;
   }
   .logo{
-    width: 10%;
-    height: 10%;
+    width: 100%;
+    height: 100%;
     border-radius: 5px;
-    float: left;
+    float: right;
     position: relative;
     top:  -50%;
     left: 10%;
@@ -96,15 +100,13 @@ export default {
     width: 90%
   }
   .buttontext{
-  font-size:18px;
+  font-size:20px;
     font-weight:bold;
   color: #ffffff;
   }
-  .r{
-    height: 24%;
-  }
+
   .hrefTexe{
-    font-size:18px;
+    font-size:20px;
 
     /*font-weight:bold;*/
     color: #ffffff;
